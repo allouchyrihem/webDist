b9dd0f60cfb7ccfab9f474d7b37824adf356bf0f
BdSalma <beddakhliasalma@gmail.com>
2024-10-27 18:37:41 +0100

    my commit

---
+++ StationVelo/src/main/java/tn/esprit/StationVelo/StationVeloApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.StationVelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationVeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationVeloApplication.class, args);
+	}
+
+}
