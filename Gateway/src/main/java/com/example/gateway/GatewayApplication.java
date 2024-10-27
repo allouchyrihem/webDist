483ffa6121331aedf5add5cfbbbb82d1a44afbf9
allouchyrihem <allouchy.ryhem@gmail.com>
2024-10-27 16:35:08 +0100

    eureka+gateway

---
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder build)
+    {
+        //hna thotou les paths mtaa vos api
+        //http://localhost:8082 hedha path les microservices mteekom ili tetruna hna juste hatit exemple
+        return build.routes().route("event",r->r.path("/hello")
+                .uri("http://localhost:8082")).build();
+    }
+}
+
